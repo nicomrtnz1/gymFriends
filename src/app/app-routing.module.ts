ce86c1d705baed56e941f767e4c59bbfe6e6ebf1
nico.martinez <nico.martinez@jbhunt.com>
2022-11-12 18:57:35 -0600

    created skeleton feature modules with lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'friends',
+    loadChildren: () =>
+      import('./friends/friends.module').then((m) => m.FriendsModule)
+  },
+  {
+    path: 'results',
+    loadChildren: () =>
+      import('./results/results.module').then((m) => m.ResultsModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
