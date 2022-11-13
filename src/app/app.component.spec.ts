e41c915a8ee71134be2595d0b42582e2b6304845
nico.martinez <nico.martinez@jbhunt.com>
2022-11-12 23:24:14 -0600

    Update app component -update verbiage and color -updated unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,17 +6,13 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-      schemas: [NO_ERRORS_SCHEMA],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   });
 
- test('should create the app', () => {
+  test('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
@@ -25,6 +21,6 @@ describe('AppComponent', () => {
   test(`should have as title 'gymFriends'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gymFriends');
+    expect(app.title).toEqual('Do I even lift?');
   });
 });
