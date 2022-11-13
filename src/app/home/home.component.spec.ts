c18d051c59c4cb2a01d106dea72b8a7644f89353
nico.martinez <nico.martinez@jbhunt.com>
2022-11-12 23:13:24 -0600

    Updated home page -added theme -added button to route to friends feature -updated unit test to test routerLink

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,23 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { HomeComponent } from './home.component';
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({ template: '' })
+class DummyComponent {}
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let location: Location;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: 'friends', component: DummyComponent }
+        ])
+      ],
+      declarations: [HomeComponent, DummyComponent]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+    location = TestBed.inject(Location);
     fixture.detectChanges();
   });
 
   test('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  test('should navigate to friends', () => {
+    const button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+
+    expect(location.path()).toBe('/friends');
   });
 });
