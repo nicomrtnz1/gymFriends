131b2f8e2cc70b48407240dd8a227dca8908bd75
nico.martinez <nico.martinez@jbhunt.com>
2022-11-15 11:46:10 -0600

    -Added d3 charts to chart componets -Updated Results page to let a user know there is no data

--- src/app/results/scatter-chart/scatter-chart.component.ts
+++ src/app/results/scatter-chart/scatter-chart.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, Input, AfterViewInit, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  AfterViewInit,
+  ViewChild
+} from '@angular/core';
 import { Friend } from 'src/app/shared/friend.interface';
 import * as d3 from 'd3';
 @Component({
@@ -7,14 +13,15 @@ import * as d3 from 'd3';
   styleUrls: ['./scatter-chart.component.scss']
 })
 export class ScatterChartComponent implements AfterViewInit {
-  @Input() data: Friend[] =[];
+  @Input() data: Friend[] = [];
   @ViewChild('scatterChart') private chartContainer!: ElementRef;
-  margin = { top: 20, right: 50, bottom: 30, left: 40 };
 
-  constructor() { }
+  constructor() {}
 
   ngAfterViewInit() {
-    if (this.data.length === 0) { return; }
+    if (this.data.length === 0) {
+      return;
+    }
 
     this.createChart();
   }
@@ -22,42 +29,65 @@ export class ScatterChartComponent implements AfterViewInit {
   private createChart() {
     const element = this.chartContainer.nativeElement;
     const data = this.data;
+    const margin = { top: 20, right: 50, bottom: 50, left: 40 };
 
-    const svg = d3.select(element).append('svg')
+    const svg = d3
+      .select(element)
+      .append('svg')
       .attr('width', element.offsetWidth)
       .attr('height', element.offsetHeight);
 
-    const contentWidth = element.offsetWidth - this.margin.left - this.margin.right;
-    const contentHeight = element.offsetHeight - this.margin.top - this.margin.bottom;
-
+    const contentWidth = element.offsetWidth - margin.left - margin.right;
+    const contentHeight = element.offsetHeight - margin.top - margin.bottom;
+    const maxWeight = d3.max(data, (d) => parseInt(d.weight)) as number;
     const x = d3
       .scaleLinear()
       .rangeRound([0, contentWidth])
-      .domain([0, d3.max(data, d => parseInt(d.age)) as number]);
+      .domain([0, (d3.max(data, (d) => parseInt(d.age)) as number) + 5]);
 
     const y = d3
       .scaleLinear()
       .rangeRound([contentHeight, 0])
-      .domain([0, d3.max(data, d => parseInt(d.weight)) as number]);
+      .domain([0, maxWeight + 15]);
 
-    const g = svg.append('g')
-      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+    const g = svg
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
+    g.append('text')
+      .attr('y', -30)
+      .attr('x', 0 - element.offsetHeight / 4)
+      .attr('text-anchor', 'middle')
+      .attr('transform', 'rotate(-90)')
+      .style('fill', 'white')
+      .text('Weight');
+    g.append('text')
+      .attr('x', element.offsetWidth / 2 - margin.left)
+      .attr('y', element.offsetHeight - 40)
+      .attr('text-anchor', 'middle')
+      .style('fill', 'white')
+      .text('Age');
     g.append('g')
       .attr('class', 'axis axis--x')
       .attr('transform', 'translate(0,' + contentHeight + ')')
       .call(d3.axisBottom(x));
 
-    g.append('g')
-      .attr('class', 'axis axis--y')
-      .call(d3.axisLeft(y));
+    g.append('g').attr('class', 'axis axis--y').call(d3.axisLeft(y));
 
     g.selectAll('.scatter')
       .data(data)
-      .enter().append('circle')
-       .attr('cx', d => x(parseInt(d.age)))
-      .attr('cy', d => y(parseInt(d.weight)))
-      .attr("r", 7)
+      .enter()
+      .append('circle')
+      .attr('fill', (d) => {
+        if (parseInt(d['weight']) > maxWeight * 0.6) {
+          return 'red';
+        } else if (parseInt(d['weight']) > maxWeight * 0.3) {
+          return 'yellow';
+        }
+        return 'green';
+      })
+      .attr('cx', (d) => x(parseInt(d.age)))
+      .attr('cy', (d) => y(parseInt(d.weight)))
+      .attr('r', 7);
   }
-
 }
--- src/app/results/weight-chart/weight-chart.component.ts
+++ src/app/results/weight-chart/weight-chart.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, Input, AfterViewInit, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  AfterViewInit,
+  ViewChild
+} from '@angular/core';
 import { Friend } from 'src/app/shared/friend.interface';
 import * as d3 from 'd3';
 
@@ -8,59 +14,83 @@ import * as d3 from 'd3';
   styleUrls: ['./weight-chart.component.scss']
 })
 export class WeightChartComponent implements AfterViewInit {
-  @Input() data: Friend[] =[];
+  @Input() data: Friend[] = [];
   @ViewChild('weightChart') private chartContainer!: ElementRef;
-  margin = { top: 20, right: 20, bottom: 30, left: 40 };
 
-  constructor() { }
+  constructor() {}
 
   ngAfterViewInit(): void {
-    if (this.data.length === 0) { return; }
+    if (this.data.length === 0) {
+      return;
+    }
     this.createChart();
   }
 
   private createChart(): void {
     const element = this.chartContainer.nativeElement;
     const data = this.data;
+    const margin = { top: 20, right: 50, bottom: 50, left: 40 };
 
-    const svg = d3.select(element).append('svg')
+    const svg = d3
+      .select(element)
+      .append('svg')
       .attr('width', element.offsetWidth)
       .attr('height', element.offsetHeight);
 
-    const contentWidth = element.offsetWidth - this.margin.left - this.margin.right;
-    const contentHeight = element.offsetHeight - this.margin.top - this.margin.bottom;
+    const contentWidth = element.offsetWidth - margin.left - margin.right;
+    const contentHeight = element.offsetHeight - margin.top - margin.bottom;
 
     const x = d3
       .scaleBand()
-      .domain(data.map(d => d.name))
+      .domain(data.map((d) => d.name))
       .rangeRound([0, contentWidth])
       .padding(0.1);
 
-    const y = d3
-      .scaleLinear()
-      .rangeRound([contentHeight, 0])
-      .domain([0, d3.max(data, d => parseInt(d.weight)) as number]);
+    const max = d3.max(data, (d) => parseInt(d.weight)) as number;
 
-    const g = svg.append('g')
-      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+    const y = d3.scaleLinear().rangeRound([contentHeight, 0]).domain([0, max]);
+
+    const g = svg
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
     g.append('g')
       .attr('class', 'axis axis--x')
       .attr('transform', 'translate(0,' + contentHeight + ')')
       .call(d3.axisBottom(x));
 
-    g.append('g')
-      .attr('class', 'axis axis--y')
-      .call(d3.axisLeft(y));
+    g.append('g').attr('class', 'axis axis--y').call(d3.axisLeft(y));
+
+    g.append('text')
+      .attr('y', -30)
+      .attr('x', 0 - element.offsetHeight / 4)
+      .attr('text-anchor', 'middle')
+      .attr('transform', 'rotate(-90)')
+      .style('fill', 'white')
+      .text('Weight');
+    g.append('text')
+      .attr('x', element.offsetWidth / 2 - margin.left)
+      .attr('y', element.offsetHeight - 40)
+      .attr('text-anchor', 'middle')
+      .style('fill', 'white')
+      .text('Friends');
 
     g.selectAll('.bar')
       .data(data)
-      .enter().append('rect')
+      .enter()
+      .append('rect')
+      .attr('fill', (d) => {
+        if (parseInt(d['weight']) > max * 0.6) {
+          return 'red';
+        } else if (parseInt(d['weight']) > max * 0.3) {
+          return 'yellow';
+        }
+        return 'green';
+      })
       .attr('class', 'bar')
-       .attr('x', d => x(d.name)as number)
-      .attr('y', d => y(parseInt(d['weight'])))
+      .attr('x', (d) => x(d.name) as number)
+      .attr('y', (d) => y(parseInt(d['weight'])))
       .attr('width', x.bandwidth())
-      .attr('height', d => contentHeight - y(parseInt(d.weight)));
+      .attr('height', (d) => contentHeight - y(parseInt(d.weight)));
   }
-
 }
